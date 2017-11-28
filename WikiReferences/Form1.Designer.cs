a01bc31da10e12f2b48d0e33bf40093b4b64a6e0
woods <woods@DESKTOP-U8QTHR4>
2017-11-27 23:13:54 -0800

    First commit.

---
+++ WikiReferences/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WikiReferences
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetReferences = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGetReferences
+            // 
+            this.btnGetReferences.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGetReferences.Location = new System.Drawing.Point(41, 49);
+            this.btnGetReferences.Name = "btnGetReferences";
+            this.btnGetReferences.Size = new System.Drawing.Size(250, 35);
+            this.btnGetReferences.TabIndex = 0;
+            this.btnGetReferences.Text = "Get References";
+            this.btnGetReferences.UseVisualStyleBackColor = true;
+            this.btnGetReferences.Click += new System.EventHandler(this.btnGetReferences_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Location = new System.Drawing.Point(41, 23);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(250, 20);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.Text = "https://en.wikipedia.org/wiki/Grammy_Award_for_Song_of_the_Year";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 550);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 676);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnGetReferences);
+            this.Name = "Form1";
+            this.Text = "Wiki References";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetReferences;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WikiReferences/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WikiReferences
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetReferences_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            // Get entered URL
+            string url = txtURL.Text;
+
+            // Create a new HtmlWeb Object and load a webpage from that object.
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument webPage = web.Load(url);
+
+            // Use the SelectNodes method to select all nodes mathing the XPath
+            // For those unfamilar with XPath, this is the full XPath Code for finding the elements.
+            /*
+              @"/html
+                /body
+                //div[@id=""content""]
+                //div[@id=""bodyContent""]
+                //div[@id=""mw-content-text""]
+                //div[@class=""mw-parser-output""]
+                //div[@class=""reflist columns references-column-width""]
+                /ol
+                //li
+                /span[@class=""reference-text""]
+                /cite
+                /a"
+             */
+            // However, you can shorthand this by using the * operator, which does most of the finding for you,
+            // and you can select explicitly where to begin parsing.
+            string path =
+                @"
+                *
+                //div[@class=""reflist columns references-column-width""]
+                /ol
+                //li
+                /span[@class=""reference-text""]
+                /cite
+                /a[1]
+                
+                ";
+
+            // Assign the found founds to a collection (HtmlNodeCollection is index-able)
+            HtmlNodeCollection references = 
+                references = webPage.DocumentNode.SelectNodes(path);
+
+            // Loop through the found nodes and print the InnerHTML (Excludes own element tag)
+            try
+            {
+                for (int i = 1; i < references.Count; i++)
+                {
+                    listBox1.Items.Add(i + ": " + references[i].InnerHtml + "\n");
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show(
+                "No reference table found.\n\n" + 
+                "Make sure that your link is a Wikipedia page, and that it contains a references section.");
+            }
+            
+        }
+    }
+}
